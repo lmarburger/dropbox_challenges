f7db42ffc70c77b3d31b416b1e93be7e6a342374
Larry Marburger <larry@marburger.cc>
2011-09-28 21:07:35 -0700

    Calculate the area of two equal square files

--- packing_spec.rb
+++ packing_spec.rb
@@ -2,7 +2,9 @@ require 'minitest/autorun'
 require 'minitest/spec'
 
 def pack(files)
-  9
+  files.inject(0) do |area, file|
+    area += file[:height] * file[:width]
+  end
 end
 
 describe 'pack' do
@@ -10,5 +12,12 @@ describe 'pack' do
     files = [{ :height => 3, :width => 3 }]
 
     pack(files).must_equal 9
+  end
+
+  it 'calculates the area of two equal square files' do
+    files = [{ :height => 3, :width => 3 },
+             { :height => 3, :width => 3 }]
+
+    pack(files).must_equal 18
   end
 end
