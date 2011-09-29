7d1a9c109abe2eef3375c66a3ba5fb09f2113108
Larry Marburger <larry@marburger.cc>
2011-09-28 21:41:36 -0700

    Let's create a class

--- packing_spec.rb
+++ packing_spec.rb
@@ -1,36 +1,38 @@
 require 'minitest/autorun'
 require 'minitest/spec'
 
-def pack(files)
-  width = files.inject(0) do |total_width, file|
-    total_width + file[:width]
-  end
+class Dropbox < Struct.new(:files)
+  def area
+    width = files.inject(0) do |total_width, file|
+      total_width + file[:width]
+    end
 
-  height = files.inject(0) do |max_height, file|
-    max_height > file[:height] ? max_height : file[:height]
-  end
+    height = files.inject(0) do |max_height, file|
+      max_height > file[:height] ? max_height : file[:height]
+    end
 
-  width * height
+    width * height
+  end
 end
 
-describe 'pack' do
+describe Dropbox do
   it 'calculates the area of a single file' do
     files = [{ :height => 3, :width => 3 }]
 
-    pack(files).must_equal 9
+    Dropbox.new(files).area.must_equal 9
   end
 
   it 'calculates the area of two equal square files' do
     files = [{ :height => 3, :width => 3 },
              { :height => 3, :width => 3 }]
 
-    pack(files).must_equal 18
+    Dropbox.new(files).area.must_equal 18
   end
 
   it 'calculates the area of two unequal square files' do
     files = [{ :height => 3, :width => 3 },
              { :height => 4, :width => 4 }]
 
-    pack(files).must_equal 28
+    Dropbox.new(files).area.must_equal 28
   end
 end
